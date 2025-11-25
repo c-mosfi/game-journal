91e95915b95ab59985ecc73153edd55e66317513
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-11-25 17:46:52 +0100

    Add authentication context

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../services/firebase/firebaseConfig.js';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(); // [user, loading, isAuthenticated]
+
+// Provider component
+export const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// check for changes on auth states
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setLoading(false);
+		});
+		return unsubscribe;
+	}, []);
+
+	const value = {
+		user,
+		loading,
+		isAuthenticated: !!user, // true if user exists
+	};
+
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	);
+};
