ad9f69706ffffbc6f752371eecff394400fdceff
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-12-02 08:59:16 +0100

    Replace the observer `onAuthStateChanged` with `onIdTokenChanged`

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onIdTokenChanged } from 'firebase/auth';
 import { auth } from '../services/firebase/firebaseConfig.js';
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
 
 	useEffect(() => {
 		// check for changes on auth states
-		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+		const unsubscribe = onIdTokenChanged(auth, (currentUser) => {
 			setUser(currentUser);
 			setLoading(false);
 		});
