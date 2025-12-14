7361f43ee454bab05f350340f1d91fec0b553ca4
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-12-14 20:58:11 +0100

    Replace emoji icons with SVGs

--- src/pages/protected/ProfilePage.jsx
+++ src/pages/protected/ProfilePage.jsx
@@ -159,8 +159,20 @@ export const ProfilePage = () => {
 					<Link
 						to={'/collections'}
 						className="bg-white rounded-lg shadow-sm p-6 hover:shadow-md transition text-left">
-						<span className="text-3xl mb-2" aria-hidden="true">
-							📚
+						<span className="mb-2" aria-hidden="true">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="size-6">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+								/>
+							</svg>
 						</span>
 						<h2 className="text-lg font-semibold text-gray-900 mb-1">
 							My Collections
@@ -173,8 +185,20 @@ export const ProfilePage = () => {
 					<Link
 						to={'/browse'}
 						className="bg-white rounded-lg shadow-sm p-6 hover:shadow-md transition text-left">
-						<span className="text-3xl mb-2" aria-hidden="true">
-							🔍
+						<span className=" mb-2" aria-hidden="true">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="size-6">
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+								/>
+							</svg>
 						</span>
 						<h2 className="text-lg font-semibold text-gray-900 mb-1">
 							Browse Games
